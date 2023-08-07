c50bf65a318b92883bb21461791b9a2aa7a427e9
Dark Man <95143027+Dark-Man747@users.noreply.github.com>
2023-08-08 01:52:32 +0400

    Ⓒ 𝙳𝚊𝚛𝚔 𝙼𝚊𝚗 𝙸𝚗𝚌.

---
+++ telebot.py
@@ -0,0 +1,24 @@
+import requests
+import telebot
+
+TELEGRAM_BOT_TOKEN = '6630514763:AAEllHZ6H_etDV6OaLMFHF0vU5iolJeBiNY'
+
+def get_chatgpt_response(text):
+    CHATGPT_API_URL = f'https://botz-openai-prod-botz-chatgpt-s78su6.mo1.mogenius.io/chatgpt?text={text}'
+    response = requests.get(CHATGPT_API_URL)
+    return response.json()['result']
+
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    user_text = message.text
+    chatgpt_response = get_chatgpt_response(user_text)
+    bot.send_message(message.chat.id, chatgpt_response)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "مرحبًا! أنا بوت ChatGPT. ارسل لي أي شيء وسأجيب لك!")
+
+
+bot.polling()
